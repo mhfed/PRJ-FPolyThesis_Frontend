cee456191eeb981ee1cd3dac5497c36e2fa89046
mhfed <nmhieu04091999@gmail.com>
2023-09-21 12:34:14 +0700

    refactor: format with prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindFunctions: ["cn", "tw"],
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['cn', 'tw'],
 };
