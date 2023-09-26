903acdabac51b0d0b4872e74ad8713b1ccf497eb
mhfed <nmhieu04091999@gmail.com>
2023-09-26 23:59:45 +0700

    feat: add some components

--- src/components/shared/toggle-mode.tsx
+++ src/components/shared/toggle-mode.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import * as React from 'react';
-import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
-
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { Button } from '@/components/ui/button';
 
 export function ModeToggle() {
@@ -14,8 +13,8 @@ export function ModeToggle() {
 
   return (
     <Button variant='outline' size='icon' onClick={handleToggleMode}>
-      <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-      <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+      <MoonIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+      <SunIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
       <span className='sr-only'>Toggle theme</span>
     </Button>
   );
